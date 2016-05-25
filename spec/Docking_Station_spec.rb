8c282baf434de9ec5a821bf59b8db5be6d90e68d
Jonathan <j_passe@live.fr>
2016-05-25 14:25:31 +0100

    11 using instance variables

--- spec/Docking_Station_spec.rb
+++ spec/Docking_Station_spec.rb
@@ -1,11 +1,24 @@
 require "Docking_Station"
 
-describe DockingStation do 
+
+describe DockingStation do
+  subject(:docking_station) { described_class.new}
+
 	it {is_expected.to respond_to :release_bike}
 
 	it "releases working bike" do
-		bike = subject.release_bike
+		bike = docking_station.release_bike
 		expect(bike).to be_working
 	end
-	
+
+  it  {is_expected.to respond_to(:dock).with(1).argument}
+
+  it {is_expected.to respond_to(:bike)}
+
+  it "returns a bike to the station" do
+  bike = Bike.new
+  subject.dock(bike)
+  expect(subject.bike).to eq bike
+  end
+
 end
