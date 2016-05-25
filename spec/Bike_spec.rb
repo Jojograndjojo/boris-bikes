c96661100f3ed5d8451b682bde01be88b6191b7f
Jonathan <j_passe@live.fr>
2016-05-25 12:04:21 +0100

    merged with Marlon

--- spec/Bike_spec.rb
+++ spec/Bike_spec.rb
@@ -3,6 +3,6 @@ require 'Bike'
 describe Bike do
 	it 'checks if bike is working' do
 		expect(subject).to respond_to :working?
-	#it { is_expected.to respond_to :working?}
+
 end
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -4,7 +4,6 @@ require 'DockingStation'
 describe DockingStation do
 	it 'responds to release_bike' do
 		expect(subject).to respond_to :release_bike
-#	expect(DockingStation.release_bike).to respond_to
 	end
 
 	it 'gets a bike' do
